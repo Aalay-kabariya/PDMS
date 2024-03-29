1e2fb1a334cc799354bf22519aac1a6561725142
YashWadhvani <yashwadhvani123@gmail.com>
2024-03-29 18:22:34 +0530

    Fetching Data from Database Fixed

--- backend/models/Medicine.js
+++ backend/models/Medicine.js
@@ -1,15 +1,18 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const medicineSchema = new mongoose.Schema({
+const medicineSchema = new mongoose.Schema(
+  {
     Name: String,
     Description: String,
-    DosageForm: String,
     Strength: String,
-    Availability: String, 
+    Availability: String,
     Photo: String,
     MRP: String,
     Key: Number,
-})
+    DosageForm: String,
+  },
+  { collection: "Medicine" }
+);
 
-const MedicineList =  mongoose.model("Medicine", medicineSchema);
-module.exports = MedicineList;
+const Medicine = mongoose.model("Medicine", medicineSchema);
+module.exports = Medicine;
--- frontend/src/components/Shop.jsx
+++ frontend/src/components/Shop.jsx
@@ -1,55 +1,55 @@
-import ShopItem from "./ShopItem"
-import Form from 'react-bootstrap/Form'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import axios from 'axios'
-import { useState, useEffect} from "react"
-function Shop () {
-    const [products, setProducts] = useState([])
+import ShopItem from "./ShopItem";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import axios from "axios";
+import { useState, useEffect } from "react";
+function Shop() {
+  const [products, setProducts] = useState([]);
 
-    useEffect(() => {
-        axios
-          .get("http://localhost:3000/shop")
-          .then((response) => {
-            setProducts(response.data);
-            console.log(response.data);
-          })
-          .catch((error) => console.log(error));
-      }, []);
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/shop")
+      .then((response) => {
+        setProducts(response.data);
+        console.log(response.data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
 
-      
-
-
-    return (
-        <div className="shop">
-            <div className="shop-header-search">
-            
-                <div className="shop-header">
-                    <h2>SHOP NOW</h2>
-                    <h6>CHECK OUT OUR PRODUCT</h6>
-                </div>
-                <div className="shop-search">
-                    <Form inline>
-                        <Row>
-                        <Col xs="auto">
-                            <Form.Control
-                            type="text"
-                            placeholder="Search"
-                            className="mr-sm-2 search-bar"
-                            />
-                        </Col>
-                        
-                        </Row>
-                    </Form>
-                    <hr />
-                </div>
-            </div>
-            <ShopItem/>
-            <ShopItem/>
-            <ShopItem/>
-            <ShopItem/>
-
+  return (
+    <div className="shop">
+      <div className="shop-header-search">
+        <div className="shop-header">
+          <h2>SHOP NOW</h2>
+          <h6>CHECK OUT OUR PRODUCT</h6>
         </div>
-    )
+        <div className="shop-search">
+          <Form inline>
+            <Row>
+              <Col xs="auto">
+                <Form.Control
+                  type="text"
+                  placeholder="Search"
+                  className="mr-sm-2 search-bar"
+                />
+              </Col>
+            </Row>
+          </Form>
+          <hr />
+        </div>
+      </div>
+      {products.map((product) => {
+        return (
+          <ShopItem
+            Name={product.Name}
+            Description={product.Description}
+            Photo={product.Photo}
+            MRP={product.MRP}
+          />
+        );
+      })}
+    </div>
+  );
 }
-export default Shop
+export default Shop;
