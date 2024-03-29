ccd380d9209e500e51999bfab49aec214cae8a61
Aalay Kabariya <111956378+Aalay-kabariya@users.noreply.github.com>
2024-03-29 18:38:17 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,11 @@
+const express = require('express')
+const connectDB = require('./dbConfig/mongoConnect')
+const mongoose = require('mongoose')
+const bodyParser=require("body-parser");
+const cors=require("cors");
+const MedicineList = require("../backend/models/Medicine")
+const morgan = require("morgan")
+const app = express()
 const express = require("express");
 const connectDB = require("./dbConfig/mongoConnect");
 const bodyParser = require("body-parser");
@@ -6,26 +14,51 @@ const Medicine = require("../backend/models/Medicine");
 const morgan = require("morgan");
 const app = express();
 
+connectDB()
 connectDB();
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use(morgan("dev"));
-
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "*");
+@@ -21,37 +20,24 @@ app.use(function (req, res, next) {
   next();
 });
 
+
+app.use(express.json()) 
+app.get( '/', (req, res) => {
+    res.send('hello')
+})
+
+mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB')
+
+})
+
+// // const medicineList = require('./routes/shop')
+// app.get("/shop", async (req,res)=> {
+//     const medicines = medicine.find()
+//     .then((medicines) => {console.log(medicines);
+//     res.json(medicines)})
+//     .catch((e) => console.log(e))
+// })
 app.use(express.json());
 app.get("/", (req, res) => {
   res.send("hello");
 });
 
 app.get("/shop", async (req, res) => {
+    try {
+        const medicines = await MedicineList.find();
+        if (!medicines) {
+            return res.status(404).json({ error: "No medicines found" });
+          }
+        res.json(medicines);
+      } catch (error) {
+        // console.log(Error.stack(error));
+        console.error("Error fetching medicines:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+      }
+  });
   try {
     const medicines = await Medicine.find({});
     console.log("Medicines found:", medicines);
@@ -40,4 +73,5 @@ app.get("/shop", async (req, res) => {
   }
 });
 
+app.listen(3000, () => console.log('Server running on port 3000'))
 app.listen(3000, () => console.log("Server running on port 3000"));
