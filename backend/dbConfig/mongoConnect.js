29dccda967fbfaed5f4158cd031d5ab49889209e
Aalay Kabariya <111956378+Aalay-kabariya@users.noreply.github.com>
2024-03-29 18:33:42 +0530

    Update mongoConnect.js

--- backend/dbConfig/mongoConnect.js
+++ backend/dbConfig/mongoConnect.js
@@ -1,6 +1,14 @@
+const mongoose = require('mongoose')
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
+    try {
+        await mongoose.connect("mongodb+srv://MedicineDatabase:medsprint@medicinecluster.9qggxhb.mongodb.net/?retryWrites=true&w=majority&appName=MedicineCluster/Pharmacy")
+        console.log('connected to mongodb')
+    } catch (err) {
+        console.log(err)
+    }
+}
   try {
     await mongoose.connect(
       "mongodb+srv://MedicineDatabase:medsprint@medicinecluster.9qggxhb.mongodb.net/Pharmacy"
@@ -11,4 +19,5 @@ const connectDB = async () => {
   }
 };
 
+module.exports = connectDB
 module.exports = connectDB;
