79c8ccd337b7311536c5a8f42a45b3861f4a7db8
YashWadhvani <yashwadhvani123@gmail.com>
2024-03-30 12:02:30 +0530

    Fixed Again

--- backend/models/Medicine.js
+++ backend/models/Medicine.js
@@ -1,25 +1,19 @@
-const mongoose = require('mongoose')
 const mongoose = require("mongoose");
 
-const medicineSchema = new mongoose.Schema({
 const medicineSchema = new mongoose.Schema(
   {
     Name: String,
     Description: String,
-    DosageForm: String,
     Strength: String,
-    Availability: String, 
+    Availability: String,
     Availability: String,
     Photo: String,
     MRP: String,
     Key: Number,
-})
     DosageForm: String,
   },
-  { collection: "Medicine" }
+  { collection: "medicines" }
 );
 
-const MedicineList =  mongoose.model("Medicine", medicineSchema);
-module.exports = MedicineList;
 const Medicine = mongoose.model("Medicine", medicineSchema);
 module.exports = Medicine;
